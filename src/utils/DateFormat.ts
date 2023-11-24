583b4cfd432e3f17ebca0378bc20a3ae8d52840f
Spyke27 <rodrigoeufrasio2@gmail.com>
2023-11-24 20:39:41 -0300

    feat: padronização de datas e horario

--- src/utils/DateFormat.ts
+++ src/utils/DateFormat.ts
@@ -1,16 +1,30 @@
 export const formatDate = (data: string) => {
     const date = new Date(data)
-    const dia = date.getDate().toString();
+    const dia = date.getDate().toString().padStart(2, '0');
     const mes = (date.getMonth()+1).toString().padStart(2, '0');
     const ano = date.getFullYear();
 
-    return dia+"/"+mes+"/"+ano;
+    const data_completa = [dia, mes, ano].join('/')
+
+    return data_completa;
 }
 
 export const formatDateTime = (data: string) => {
     const date = new Date(data)
-    const hora = date.getHours();
-    const minuto = date.getMinutes();
+    const horaF = date.getHours() -2; //Horário de Brasília
+    const hora = horaF.toString().padStart(2, '0');
+    const minuto = date.getMinutes().toString().padStart(2, '0');
 
-    return hora+":"+minuto;
+    const horas = [hora, minuto].join(':');
+
+    return horas;
 }
+
+export const convertHour = (minutos: number) => {
+    const horas = Math.floor(minutos/ 60);          
+    const min = minutos % 60;
+    const textoHoras = (`00${horas}`).slice(-2);
+    const textoMinutos = (`00${min}`).slice(-2);
+    
+    return `${textoHoras }:${textoMinutos}`;
+  };
