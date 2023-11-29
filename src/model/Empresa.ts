74f720eb22cd96b71fe3056f22fedff71de8d0d4
Spyke27 <rodrigoeufrasio2@gmail.com>
2023-11-29 20:16:33 -0300

    update: model empresa

--- src/model/Empresa.ts
+++ src/model/Empresa.ts
@@ -1,3 +1,6 @@
+import User from "./User";
+import Endereco from "./Endereco";
+
 interface Empresa {
     id: number;
     cnpj: string;
@@ -7,6 +10,12 @@ interface Empresa {
     cadastro: string;
     sobre: string;
     logo?: string | null;
+    site?: string | null;
+    instagram?: string | null;
+
+
+    Users?: User[] | null;
+    Endereco?: Endereco | null;
 }
 
 export default Empresa;
