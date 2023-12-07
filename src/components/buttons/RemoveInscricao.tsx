a663c47763a4daaf734d350d6c06298294cf7dc0
Spyke27 <rodrigoeufrasio2@gmail.com>
2023-12-06 21:46:41 -0300

    feat: add button remover inscrição

---
+++ src/components/buttons/RemoveInscricao.tsx
@@ -0,0 +1,39 @@
+//import { Link } from "react-router-dom"
+import { api } from "../../service/Service"
+import { useParams } from "react-router-dom"
+import { toast } from 'react-toastify';
+
+function RemoveAssociacao(){
+    const params = useParams()
+
+    const handleClick = async () => {
+        try {
+            await api.delete(`/vagas/deletar/inscricao/${params.id}`)
+
+            toast.success('Inscrição removida!', {
+                position: "top-center",
+                autoClose: 2000,
+                hideProgressBar: true,
+                closeOnClick: true,
+                pauseOnHover: true,
+                draggable: true,
+                progress: undefined,
+                theme: "light",
+                });     
+                location.reload()       
+        } catch (error) {
+            console.log(error)
+        }
+    }
+
+    return(
+        <>
+        <button
+            onClick={handleClick}
+            className="flex justify-center items-center bg-verde-300 px-8 py-2 rounded-md w-72 md:w-80 text-white
+            hover:bg-verde-300/80">Remover inscrição
+        </button>
+        </>
+    )
+
+}  export default RemoveAssociacao
