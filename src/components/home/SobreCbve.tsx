dae214b13a3a260588dd73bc266b6129a555b3a2
Spyke27 <rodrigoeufrasio2@gmail.com>
2023-12-15 22:02:10 -0300

    feat: add component sobre

---
+++ src/components/home/SobreCbve.tsx
@@ -0,0 +1,20 @@
+
+function SobreCbve(){
+    return(
+    <>
+    <section className="flex flex-col items-center justify-center bg-roxo-500 p-10 gap-5">
+        <h2 className="text-white text-xl font-bold">SOBRE O CBVE</h2>
+        <p className="text-cinza-100 text-lg text-center px-48">
+        O CBVE é uma rede de parceiros de grande referência em voluntariado e espaço de inovação para compartilhamento e criação de experiências.
+        Nossos encontros e trocas geram conteúdo relevante sobre voluntariado empresarial que compartilhamos com a sociedade. Inspiramos outras pessoas e organizações a alinharem esforços em nome de um futuro compartilhado que não deixe ninguém para trás, considerando a Agenda 2030 e os ODS, e os critérios ESG (Environmental, Social and Governance)
+        Seguimos colaborando para que estes programas impactem comunidades, engajem colaboradores, promovam marcas e inspirem ainda mais…
+        </p>
+
+        <iframe width="560" height="315" src="https://www.youtube.com/embed/PCIhBqkwIvw?si=YtGyz53uc1yjz1md"      title="CBVE" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowFullScreen>
+        </iframe>
+    </section>
+    </>
+    )
+}
+
+export default SobreCbve
