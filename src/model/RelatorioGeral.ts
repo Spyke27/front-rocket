6dc811cd1aa3fa3da5815f99b260992fff293123
Spyke27 <rodrigoeufrasio2@gmail.com>
2024-01-18 21:24:22 -0300

    feat: add models relatorios

---
+++ src/model/RelatorioGeral.ts
@@ -0,0 +1,13 @@
+interface RelatorioGeral {
+    pessoasImpactadas: number;
+    tempoVoluntariado: number;
+    vagas: number;
+    usuarios: number;
+    empresas: number;
+    instituicoes: number;
+    causa: string;
+    ods: string;
+    politica: string;
+}
+
+export default RelatorioGeral;
